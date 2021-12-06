de0a9e32bcf3137ed68a98689133adb4d27d1b58
Shelli <sh.panjieva@gmail.com>
2021-12-06 09:43:31 +0900

    first

---
+++ index.js
@@ -0,0 +1,85 @@
+var title = "jon";
+var title1 = title + "it academy";
+var title2 = "webbrain 'IT' Academy 'IT'";
+var str = `IT ${title} IT Academy`
+// console.log(title2);
+// console.log(str.indexOf('IT'));
+// console.log(str.lastIndexOf('IT'));
+// console.log(str.indexOf('IT' ,5));
+// console.log(str);
+// console.log(title2.concat(title1));
+// console.log(title2.slice(0,4));
+// console.log(title2.substring(2,5));
+// console.log(title2.substr(2,5));
+// console.log(parseInt(1.2588555));
+// console.log(title2.lastIndexOf('IT' ,20));
+// console.log(title.length);
+// console.log(title2.replace('IT', 'Xon'));
+// console.log(title2.replace(/'IT'/gi, 'Xon'));
+fruits = 'Orange,Apple,Grap'
+
+
+// console.log(fruits.split(','));
+
+
+// havo = 'sovuq';
+
+// if(havo === 'issiq'){
+//     console.log('bugun issiq');
+// }
+// else if(havo==='salgina'){
+//     console.log(havo='sovuq');
+// }
+// else if(havo==='sovuq'){
+//     console.log(havo='qor');
+// }
+
+
+
+// function Sum(name){
+//     console.log('name:', 'Sardor', name);
+//     console.log('surname:','Muktorov')
+// }
+
+// Sum('sar');
+// Sum('kor');
+
+// Sum('bor');
+
+
+// function pow(a,b){
+//     return a**b;
+// }
+// console.log(pow(2,3));
+
+
+// const sqrt = (a) =>  a**0.5;
+
+// console.log(sqrt(49));
+
+// havo = 'quyosh'
+
+// switch(havo){
+//     case 'bulut':console.log('havo bulut');
+//     break;
+//     case 'yomgir':console.log('havo yomgir');
+//     break;
+//     case 'quyosh':console.log('havo quyosh');
+//     // break;
+// default:
+//     console.log('mavjudmas');
+//     break;
+// }
+
+const step = (i,name='step' ) => {
+    console.log(name,i);
+    };
+    
+for(let i=0; i<=10; i++){
+    // console.log(i, 'name')
+    step(i,'jon');
+}
+
+
+
+
